d81610711fd1edf9568f71b3c3ef140d0c52c075
查郁 <zhayu.ll@taobao.com>
2017-03-16 11:34:12 +0800

    更新demo

--- atlas-demo/app/src/main/java/com/taobao/demo/update/Updater.java
+++ atlas-demo/app/src/main/java/com/taobao/demo/update/Updater.java
@@ -213,13 +213,15 @@ import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
 import android.widget.Toast;
-
 import com.alibaba.fastjson.JSON;
 import com.taobao.atlas.dex.util.FileUtils;
-import com.taobao.atlas.update.AtlasUpdater;
+import com.taobao.atlas.dexmerge.MergeCallback;
 import com.taobao.atlas.update.model.UpdateInfo;
+import com.taobao.atlas.update.util.PatchInstaller;
+import com.taobao.atlas.update.util.PatchMerger;
 
 import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by wuzhong on 2016/12/20.
@@ -227,7 +229,7 @@ import java.io.File;
 
 public class Updater {
 
-    public static void update(Context context) {
+    public static void update(final Context context) {
 
         File updateInfo = new File(context.getExternalCacheDir(), "update.json");
 
@@ -243,7 +245,30 @@ public class Updater {
         File patchFile = new File(context.getExternalCacheDir(), "patch-" + info.updateVersion + "@" + info.baseVersion + ".tpatch");
 
         try {
-            AtlasUpdater.update(info, patchFile);
+//            AtlasUpdater.update(info, patchFile);
+
+            PatchMerger patchMerger = new PatchMerger(info, patchFile, new MergeCallback() {
+                @Override
+                public void onMergeResult(boolean result, String bundleName) {
+                    if (result) {
+                        toast(bundleName + "merge success!", context);
+                    }else {
+                        toast(bundleName + "merge failed!", context);
+
+                    }
+                }
+            });
+
+            try {
+                patchMerger.merge();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Log.e("mergeOutputs:",String.valueOf(patchMerger.mergeOutputs.size()));
+            PatchInstaller patchInstaller = new PatchInstaller(patchMerger.mergeOutputs, info);
+
+            patchInstaller.install();
+
             Log.e("update", "update success");
             toast("更新成功，请重启app", context);
         } catch (Throwable e) {
