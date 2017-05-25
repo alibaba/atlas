0fbfd6e9fb74d6b93601d5df57857d00a255ec93
chenzhong.cz <chenzhong.cz@alibaba-inc.com>
2017-05-25 10:28:12 +0800

    使用新版插件

--- atlas-demo/AtlasDemo/app/src/main/java/com/taobao/update/Updater.java
+++ atlas-demo/AtlasDemo/app/src/main/java/com/taobao/update/Updater.java
@@ -208,18 +208,18 @@
 
 package com.taobao.update;
 
+import java.io.File;
+
+import com.alibaba.fastjson.JSON;
+
 import android.content.Context;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
 import android.widget.Toast;
-
-import com.alibaba.fastjson.JSON;
 import com.taobao.atlas.dex.util.FileUtils;
 import com.taobao.atlas.update.AtlasUpdater;
 import com.taobao.atlas.update.model.UpdateInfo;
-
-import java.io.File;
 
 /**
  * Created by wuzhong on 2016/12/20.
@@ -227,16 +227,23 @@ import java.io.File;
 
 public class Updater {
 
-    public static final String TAG = "Updater";
-
-
     public static void update(Context context) {
-        UpdateInfo info = loadUpdateInfo(context);
-        if (null == info) return;
-
-        File patchFile = new File(context.getExternalCacheDir(), "patch-" + info.updateVersion + "@" + info.baseVersion + ".tpatch");
 
         try {
+            String versionName = context.getPackageManager().getPackageInfo(context.getPackageName(), 0).versionName;
+            File updateInfo = new File(context.getExternalCacheDir(), "update-" + versionName + ".json");
+
+            if (!updateInfo.exists()) {
+                Log.e("update", "更新信息不存在，请先 执行 buildTpatch.sh");
+                toast("更新信息不存在，请先 执行 buildTpatch.sh", context);
+                return;
+            }
+
+            String jsonStr = new String(FileUtils.readFile(updateInfo));
+            UpdateInfo info = JSON.parseObject(jsonStr, UpdateInfo.class);
+
+            File patchFile = new File(context.getExternalCacheDir(),
+                                      "patch-" + info.updateVersion + "@" + info.baseVersion + ".tpatch");
             AtlasUpdater.update(info, patchFile);
             Log.e("update", "update success");
             toast("更新成功，请重启app", context);
@@ -244,8 +251,6 @@ public class Updater {
             e.printStackTrace();
             toast("更新失败, " + e.getMessage(), context);
         }
-
-
     }
 
 
@@ -257,6 +262,7 @@ public class Updater {
             }
         });
     }
+<<<<<<< ae02cf0e0df8da103c8fb4c6a4702bf0c4dfa7d5
 
     public static void dexPatchUpdate(Context context) {
         UpdateInfo info = loadUpdateInfo(context);
@@ -299,4 +305,6 @@ public class Updater {
         return JSON.parseObject(jsonStr, UpdateInfo.class);
     }
 
+=======
+>>>>>>> 使用新版插件
 }
