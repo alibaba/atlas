f8e2204c7316de632bf76b9299b8c55ff4152349
查郁 <zhayu.ll@taobao.com>
2017-05-27 10:49:46 +0800

    [atlas-gradle-plugin]add dexconfigs

---
+++ atlas-gradle-plugin/atlas-plugin/src/main/java/com/taobao/android/builder/extension/DexConfig.java
@@ -0,0 +1,46 @@
+package com.taobao.android.builder.extension;
+
+/**
+ * @author lilong
+ * @create 2017-05-26 下午9:48
+ */
+
+public class DexConfig {
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+    public DexConfig(String name){
+        this.name = name;
+    }
+
+    private boolean useMyDex = false;
+
+    public boolean isUseMyDex() {
+        return useMyDex;
+    }
+
+    public void setUseMyDex(boolean useMyDex) {
+        this.useMyDex = useMyDex;
+    }
+
+    public boolean isDexInProcess() {
+        return dexInProcess;
+    }
+
+    public void setDexInProcess(boolean dexInProcess) {
+        this.dexInProcess = dexInProcess;
+    }
+
+    private boolean dexInProcess = false;
+
+
+
+}
---
+++ atlas-gradle-plugin/atlas-plugin/src/main/java/com/taobao/android/builder/extension/factory/DexConfigFactory.java
@@ -0,0 +1,35 @@
+package com.taobao.android.builder.extension.factory;
+
+import com.android.annotations.NonNull;
+import com.taobao.android.builder.extension.DexConfig;
+import org.gradle.api.NamedDomainObjectFactory;
+import org.gradle.api.Project;
+import org.gradle.api.logging.Logger;
+import org.gradle.internal.reflect.Instantiator;
+
+/**
+ * @author lilong
+ * @create 2017-05-26 下午9:48
+ */
+
+public class DexConfigFactory implements NamedDomainObjectFactory<DexConfig> {
+
+    @NonNull
+    private final Instantiator instantiator;
+    @NonNull
+    private final Project project;
+
+    @NonNull
+    private final Logger logger;
+
+    public DexConfigFactory(@NonNull Instantiator instantiator, @NonNull Project project, @NonNull Logger logger) {
+        this.instantiator = instantiator;
+        this.project = project;
+        this.logger = logger;
+    }
+
+    @Override
+    public DexConfig create(String name) {
+        return instantiator.newInstance(DexConfig.class, name);
+    }
+}
