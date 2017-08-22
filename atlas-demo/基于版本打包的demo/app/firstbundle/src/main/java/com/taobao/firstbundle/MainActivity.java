4e151bd6f56f632f75b1ccd13a477d07a09cab80
zhongcang <peihan.cph@alibaba-inc.com>
2017-08-22 14:53:42 +0800

    修改 demo,  case xxx 改成 if 判断

--- atlas-demo/基于版本打包的demo/app/firstbundle/src/main/java/com/taobao/firstbundle/MainActivity.java
+++ atlas-demo/基于版本打包的demo/app/firstbundle/src/main/java/com/taobao/firstbundle/MainActivity.java
@@ -16,16 +16,16 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
+
+            if (item.getItemId() == R.id.navigation_home){
+                mTextMessage.setText(R.string.title_home);
+                return true;
+            }else if(item.getItemId() == R.id.navigation_dashboard){
+                mTextMessage.setText(R.string.title_dashboard);
+                return true;
+            }else if(item.getItemId() == R.id.navigation_notifications){
+                mTextMessage.setText(R.string.title_notifications);
+                return true;
             }
             return false;
         }
