8504aa1eae5a2489b9c8bdb3434a1a786aa434fc
陈佩翰 <chenpeihan@ali-186590cd286d.local>
2017-09-04 22:02:38 +0800

    建立base工程

---
+++ atlas-demo/demo_case/base_case/components/maindex/src/main/java/com/taobao/atlas/base/maindex/DemoActivity.java
@@ -0,0 +1,18 @@
+package com.taobao.atlas.base.maindex;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 种藏 on 2017/9/4.
+ */
+
+public class DemoActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
---
+++ atlas-demo/demo_case/base_case/components/maindex/src/main/java/com/taobao/atlas/base/maindex/DemoApplication.java
@@ -0,0 +1,20 @@
+package com.taobao.atlas.base.maindex;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Created by zhongcang on 2017/9/4
+ */
+
+public class DemoApplication extends Application {
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
