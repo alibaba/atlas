0f27da26c1684595ce8db40084cd90c5da2f397b
zhongcang <peihan.cph@alibaba-inc.com>
2017-09-05 17:22:06 +0800

    demo 2 更新

---
+++ atlas-demo/demo_case/bundle_complex_case/componets/maindex/src/main/java/com/taobao/atlas/complex/maindex/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.taobao.atlas.complex.maindex;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Created by zhongcnag on 2017/9/5.
+ * .
+ */
+
+public class DemoApplication extends Application {
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ atlas-demo/demo_case/bundle_complex_case/componets/maindex/src/main/java/com/taobao/atlas/complex/maindex/MainActivity.java
@@ -0,0 +1,11 @@
+package com.taobao.atlas.complex.maindex;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by zhongcang on 2017/9/5.
+ * .
+ */
+
+public class MainActivity extends AppCompatActivity {
+}
