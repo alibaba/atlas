42ae9e165a37183cead0f05f2926ac281a42827a
chenzhong.cz <chenzhong.cz@alibaba-inc.com>
2017-09-08 13:12:52 +0800

    [atlas-gradle-plugin] 兼容开源插件

--- atlas-gradle-plugin/atlas-plugin/src/main/java/com/taobao/android/builder/tasks/incremental/ParsedModuleStringNotation.java
+++ atlas-gradle-plugin/atlas-plugin/src/main/java/com/taobao/android/builder/tasks/incremental/ParsedModuleStringNotation.java
@@ -16,9 +16,11 @@ public class ParsedModuleStringNotation {
     public ParsedModuleStringNotation(String moduleNotation) {
         int idx = moduleNotation.lastIndexOf('@');
         if (idx == -1) {
+            assignValuesFromMtlModuleNotation(moduleNotation);
+        } else {
             assignValuesFromModuleNotation(moduleNotation);
+            this.artifactType = moduleNotation.substring(idx + 1);
         }
-        this.artifactType = moduleNotation.substring(idx + 1);
     }
 
     private void assignValuesFromModuleNotation(String moduleNotation) {
@@ -35,6 +37,42 @@ public class ParsedModuleStringNotation {
         }
         assignValue(count, moduleNotation.substring(idx, cur));
         count++;
+        if (count < 2 || count > 4) {
+            throw new IllegalDependencyNotation("Supplied String module notation '" + moduleNotation
+                + "' is invalid. Example notations: 'org.gradle:gradle-core:2.2', 'org.mockito:mockito-core:1.9.5:javadoc'.");
+        }
+    }
+
+    private void assignValue(int count, String fragment) {
+        switch (count) {
+            case 0:
+                group = "".equals(fragment) ? null : fragment;
+                break;
+            case 1:
+                name = fragment;
+                break;
+            case 2:
+                version = "".equals(fragment) ? null : fragment;
+                break;
+            case 3:
+                classifier = fragment;
+        }
+    }
+
+    private void assignValuesFromMtlModuleNotation(String moduleNotation) {
+        int count = 0;
+        int idx = 0;
+        int cur = -1;
+        while (++cur < moduleNotation.length()) {
+            if (':' == moduleNotation.charAt(cur)) {
+                String fragment = moduleNotation.substring(idx, cur);
+                assignMtlValue(count, fragment);
+                idx = cur + 1;
+                count++;
+            }
+        }
+        assignMtlValue(count, moduleNotation.substring(idx, cur));
+        count++;
         if (count < 2 || count > 5) {
             throw new IllegalDependencyNotation("Supplied String module notation '" + moduleNotation
                 + "' is invalid. Example notations: 'org.gradle:gradle-core:2.2', "
@@ -42,7 +80,7 @@ public class ParsedModuleStringNotation {
         }
     }
 
-    private void assignValue(int count, String fragment) {
+    private void assignMtlValue(int count, String fragment) {
         switch (count) {
             case 0:
                 group = "".equals(fragment) ? null : fragment;
