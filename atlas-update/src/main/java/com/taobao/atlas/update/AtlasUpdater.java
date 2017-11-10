62760e759e60fce3ab6f257a9d09e5915a2a52bf
zhongcang <peihan.cph@alibaba-inc.com>
2017-11-10 21:11:56 +0800

    逻辑简化

--- atlas-update/src/main/java/com/taobao/atlas/update/AtlasUpdater.java
+++ atlas-update/src/main/java/com/taobao/atlas/update/AtlasUpdater.java
@@ -1,26 +1,20 @@
 package com.taobao.atlas.update;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
 import android.content.Context;
-import android.taobao.atlas.versionInfo.BaselineInfoManager;
+import android.taobao.atlas.runtime.RuntimeVariables;
 import android.text.TextUtils;
 import android.util.Log;
-import android.util.Pair;
-
 import com.taobao.atlas.dexmerge.MergeCallback;
 import com.taobao.atlas.update.exception.MergeException;
 import com.taobao.atlas.update.model.UpdateInfo;
 import com.taobao.atlas.update.util.PatchCleaner;
 import com.taobao.atlas.update.util.PatchInstaller;
 import com.taobao.atlas.update.util.PatchMerger;
-
 import org.osgi.framework.BundleException;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by wuzhong on 2016/11/23.
@@ -28,18 +22,17 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class AtlasUpdater {
 
-    private static boolean usePatchDivider = true;
-
     /**
      * 更新主入口
-     * @param updateInfo  更新的基础信息
-     * @param patchFile   tpatch包
+     *
+     * @param updateInfo 更新的基础信息
+     * @param patchFile  tpatch包
      * @throws MergeException
      * @throws BundleException
      */
     public static void update(UpdateInfo updateInfo, File patchFile) throws MergeException, BundleException {
 
-        if (null == updateInfo || updateInfo.dexPatch){
+        if (null == updateInfo || updateInfo.dexPatch) {
             //with dexPatch,please call "dexPatchUpdate"
             return;
         }
@@ -54,7 +47,6 @@ public class AtlasUpdater {
                 }
             }
         };
-
 
         PatchMerger patchMerger = new PatchMerger(updateInfo, patchFile, mergeCallback);
 
@@ -72,92 +64,45 @@ public class AtlasUpdater {
 
     }
 
-    public static void dividePatch(boolean divide) {
-        usePatchDivider = divide;
-    }
+    public static void dexpatchUpdate(Context context, UpdateInfo updateInfo, File patchFile,
+                                      final IDexpatchMonitor coldMonitor, boolean enableHot,
+                                      IDexpatchMonitor hotMonitor)
+        throws Exception {
 
-
-    public static void dexpatchUpdate(Context context, UpdateInfo updateInfo, File patchFile, final IDexpatchMonitor monitor) throws Exception {
-
-        if (null == updateInfo || !updateInfo.dexPatch){
+        if (null == updateInfo || !updateInfo.dexPatch) {
             return;
         }
-        //检查版本，只有versionName相等
-        //并所有bundle的dexpatchVersion都大于对应bundle的dexpatchbundle时，才会执行dexpatch操作
         String versionName = null;
         try {
-            versionName = context.getPackageManager().getPackageInfo(context.getPackageName(), 0).versionName;
+            Context c = RuntimeVariables.androidApplication;
+            versionName = c.getPackageManager().getPackageInfo(c.getPackageName(), 0).versionName;
         } catch (Exception e) {
             e.printStackTrace();
         }
         if (TextUtils.isEmpty(versionName) || !versionName.equals(updateInfo.baseVersion)) {
             return;
         }
-        if (usePatchDivider) {
-            updateInfo.updateBundles = DexPatchDivider.getColdPatchList(updateInfo.updateBundles);
-        }
 
-        Iterator<UpdateInfo.Item> itemIterator = updateInfo.updateBundles.iterator();
-        while (itemIterator.hasNext()) {
-            UpdateInfo.Item item = itemIterator.next();
-            long bundleVersion = BaselineInfoManager.instance().getDexPatchBundleVersion(item.name);
-            if (item.dexpatchVersion < bundleVersion || (item.dexpatchVersion == bundleVersion && !item.reset)) {
-                itemIterator.remove();
-            }
+        if (enableHot) {
+            List<UpdateInfo.Item> needPatchHotBundles = DexPatchUpdater.filterNeedHotPatchList(
+                DexPatchUpdater.dividePatchInfo(updateInfo.updateBundles, true)
+            );
+            DexPatchUpdater.installHotPatch(updateInfo.updateVersion, needPatchHotBundles, patchFile, hotMonitor);
         }
-        if (updateInfo.updateBundles.isEmpty()){
-            return;
-        }
+        updateInfo.updateBundles = DexPatchUpdater.filterNeedHotPatchList(
+            DexPatchUpdater.dividePatchInfo(updateInfo.updateBundles, false)
+        );
+        DexPatchUpdater.installColdPatch(updateInfo, patchFile, coldMonitor);
+    }
 
-        //开始merge
-        PatchMerger patchMerger = null;
-        try {
-            patchMerger = new PatchMerger(updateInfo, patchFile, null);
-            patchMerger.merge();
-
-        if (patchMerger == null) {
-            return;
-        } else {
-            List<UpdateInfo.Item> result = new ArrayList<>();
-            for (int i = 0; i < updateInfo.updateBundles.size(); i++) {
-                UpdateInfo.Item item = updateInfo.updateBundles.get(i);
-                if (patchMerger.mergeOutputs.containsKey(item.name)) {//对于merge成功的bundle列表进行更新
-                    Pair<String, UpdateInfo.Item> pair = patchMerger.mergeOutputs.get(item.name);
-                    boolean succeed = new File(pair.first).exists();
-                    if (succeed) {
-                        result.add(item);
-                    }
-                    if (monitor != null) {
-                        monitor.merge(succeed, item.name, item.dexpatchVersion, "");
-                    }
-                }
-            }
-            updateInfo.updateBundles = result;
-        }
-
-            PatchInstaller patchInstaller = new PatchInstaller(patchMerger.mergeOutputs, updateInfo);
-            patchInstaller.install();
-        } catch (Exception e) {
-                throw e;
-        }
-
-        ConcurrentHashMap<String, Long> installList = BaselineInfoManager.instance().getDexPatchBundles();
-        if (installList == null || installList.size() == 0) {
-            return;
-        }
-
-        if (null != monitor){
-            for (UpdateInfo.Item item : updateInfo.updateBundles){
-                boolean succeed = installList.containsKey(item.name) && item.dexpatchVersion == installList.get(item.name);
-                monitor.install(succeed,item.name, item.dexpatchVersion, "");
-            }
-        }
-
-        PatchCleaner.clearUpdatePath(updateInfo.workDir.getAbsolutePath());
+    public static void dexpatchUpdate(Context context, UpdateInfo updateInfo, File patchFile,
+                                      final IDexpatchMonitor monitor) throws Exception {
+        dexpatchUpdate(context, updateInfo, patchFile, monitor, false, null);
     }
 
     public interface IDexpatchMonitor {
         public void merge(boolean success, String bundleName, long version, String errMsg);
+
         public void install(boolean success, String bundleName, long version, String errMsg);
     }
 
---
+++ atlas-update/src/main/java/com/taobao/atlas/update/DexPatchUpdater.java
@@ -0,0 +1,191 @@
+package com.taobao.atlas.update;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.zip.ZipFile;
+
+import android.content.Context;
+import android.taobao.atlas.patch.AtlasHotPatchManager;
+import android.taobao.atlas.runtime.RuntimeVariables;
+import android.taobao.atlas.util.IOUtil;
+import android.taobao.atlas.versionInfo.BaselineInfoManager;
+import android.text.TextUtils;
+import android.util.Pair;
+import com.taobao.atlas.update.AtlasUpdater.IDexpatchMonitor;
+import com.taobao.atlas.update.model.UpdateInfo;
+import com.taobao.atlas.update.model.UpdateInfo.Item;
+import com.taobao.atlas.update.util.PatchCleaner;
+import com.taobao.atlas.update.util.PatchInstaller;
+import com.taobao.atlas.update.util.PatchMerger;
+
+/**
+ * Created by zhongcang on 2017/11/7.
+ * .
+ */
+
+public class DexPatchUpdater {
+
+    public static void installHotPatch(String updateVersion,List<UpdateInfo.Item> updateList, File patchFile,IDexpatchMonitor monitor) {
+        if (null == updateList || updateList.isEmpty()){
+            return;
+        }
+        ZipFile patchZip = null;
+        try {
+            patchZip = new ZipFile(patchFile);
+            HashMap<String, Pair<Long, InputStream>> updateBundles = new HashMap<>(updateList.size());
+            for (UpdateInfo.Item bundle : updateList) {
+                String entryName = "com.taobao.maindex".equals(bundle.name)
+                    ? "hotfix.dex"
+                    : String.format("%s%s/hotfix.dex", "lib", bundle.name.replace(".", "_"));
+                InputStream entryIn = patchZip.getInputStream(patchZip.getEntry(entryName));
+                updateBundles.put(bundle.name, new Pair<>(bundle.hotPatchVersion, entryIn));
+            }
+            AtlasHotPatchManager.getInstance().installHotFixPatch(updateVersion, updateBundles);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            IOUtil.quietClose(patchZip);
+        }
+        if (null != monitor) {
+            Map<String, Long> hotPatchInstall = AtlasHotPatchManager.getInstance().getAllInstallPatch();
+            if (null == hotPatchInstall) {
+                hotPatchInstall = new HashMap<>(1);
+            }
+            for (UpdateInfo.Item item : updateList) {
+                boolean success = hotPatchInstall.containsKey(item.name);
+                monitor.install(success, item.name, item.hotPatchVersion, "");
+            }
+        }
+    }
+
+    public static void installColdPatch(UpdateInfo updateInfo, File patchFile,IDexpatchMonitor monitor) {
+        if (null == updateInfo.updateBundles || updateInfo.updateBundles.isEmpty()){
+            return;
+        }
+        PatchMerger patchMerger = null;
+        try {
+            patchMerger = new PatchMerger(updateInfo, patchFile, null);
+            patchMerger.merge();
+
+            List<UpdateInfo.Item> result = new ArrayList<>();
+            for (int i = 0; i < updateInfo.updateBundles.size(); i++) {
+                UpdateInfo.Item item = updateInfo.updateBundles.get(i);
+                if (patchMerger.mergeOutputs.containsKey(item.name)) {
+                    //对于merge成功的bundle列表进行更新
+                    Pair<String, Item> pair = patchMerger.mergeOutputs.get(item.name);
+                    boolean succeed = new File(pair.first).exists();
+                    if (succeed) {
+                        result.add(item);
+                    }
+                    if (monitor != null) {
+                        monitor.merge(succeed, item.name, item.dexpatchVersion, "");
+                    }
+                }
+            }
+            updateInfo.updateBundles = result;
+
+            PatchInstaller patchInstaller = new PatchInstaller(patchMerger.mergeOutputs, updateInfo);
+            patchInstaller.install();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            PatchCleaner.clearUpdatePath(updateInfo.workDir.getAbsolutePath());
+        }
+
+        ConcurrentHashMap<String, Long> installList = BaselineInfoManager.instance().getDexPatchBundles();
+        if (installList == null || installList.size() == 0) {
+            return;
+        }
+
+        if (null != monitor) {
+            for (UpdateInfo.Item item : updateInfo.updateBundles) {
+                boolean succeed = installList.containsKey(item.name) && item.dexpatchVersion == installList.get(
+                    item.name);
+                monitor.install(succeed, item.name, item.dexpatchVersion, "");
+            }
+        }
+    }
+
+    public static List<Item> dividePatchInfo(List<Item> allItem, boolean hot) {
+        List<UpdateInfo.Item> list = new ArrayList<>();
+        if (null == allItem) {
+            return list;
+        }
+        int type = hot ? Item.PATCH_DEX_HOT : Item.PATCH_DEX_COLD;
+        for (UpdateInfo.Item item : allItem) {
+            if (item.patchType == type || item.patchType == Item.PATCH_DEX_C_AND_H) {
+                Item hotItem = Item.makeCopy(item);
+                hotItem.patchType = type;
+                list.add(hotItem);
+            }
+        }
+        return list;
+    }
+
+    /**
+     * @return hotPatch的数据
+     */
+    public static List<UpdateInfo.Item> filterNeedHotPatchList(List<Item> hotPatchList) {
+        List<Item> resultList = new ArrayList<>(hotPatchList.size());
+        Map<String, Long> installMap = AtlasHotPatchManager.getInstance().getAllInstallPatch();
+        if (null == installMap) {
+            installMap = new HashMap<>(1);
+        }
+        for (Item item : hotPatchList) {
+            if (item.patchType != Item.PATCH_DEX_HOT) {
+                continue;
+            }
+            Long version = installMap.get(item.name);
+
+            if (item.hotPatchVersion == -1) {
+                //本地已经安装过，并且处于未回滚状态
+                if (null != version && version != -1) {
+                    resultList.add(Item.makeCopy(item));
+                }
+                continue;
+            }
+            if (null == version || item.hotPatchVersion > version) {
+                resultList.add(Item.makeCopy(item));
+            }
+        }
+        return resultList;
+    }
+
+
+    /**
+     * @return 普通DexPatch (cold)的数据
+     */
+    public static List<UpdateInfo.Item> filterNeedColdPatchList(List<Item> coldPatchList) {
+        List<Item> resultList = new ArrayList<>(coldPatchList.size());
+        Map<String, Long> installMap = BaselineInfoManager.instance().getDexPatchBundles();
+        if (null == installMap) {
+            installMap = new HashMap<>(1);
+        }
+        for (Item item : coldPatchList) {
+            if (item.patchType != Item.PATCH_DEX_COLD) {
+                continue;
+            }
+            Long version = installMap.get(item.name);
+
+            if (item.reset) {
+                //本地已经安装过，并且处于未回滚状态
+                if (null != version && version != -1) {
+                    resultList.add(Item.makeCopy(item));
+                }
+                continue;
+            }
+            if (null == version || item.dexpatchVersion > version) {
+                resultList.add(Item.makeCopy(item));
+            }
+        }
+        return resultList;
+    }
+
+}
