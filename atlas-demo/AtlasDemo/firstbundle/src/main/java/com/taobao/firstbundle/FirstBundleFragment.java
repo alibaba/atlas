80d89a5583a66f96ebbd4d13b29441ebb8262256
baiyi <baiyi.hwj@taobao.com>
2018-04-12 16:21:16 +0800

    [atlas-demo] use remote fragment for main page for Android P

---
+++ atlas-demo/AtlasDemo/firstbundle/src/main/java/com/taobao/firstbundle/FirstBundleFragment.java
@@ -0,0 +1,43 @@
+package com.taobao.firstbundle;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.taobao.atlas.remote.IRemote;
+import android.taobao.atlas.remote.IRemoteTransactor;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.taobao.firstbundle.fragment.BlankFragment;
+import com.taobao.publicBundle.Tools;
+
+public class FirstBundleFragment extends Fragment implements IRemote {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_blank, container, false);
+        return view;
+    }
+
+
+    @Override
+    public Bundle call(String commandName, Bundle args, IResponse callback) {
+        return null;
+    }
+
+    @Override
+    public <T> T getRemoteInterface(Class<T> interfaceClass, Bundle args) {
+        return null;
+    }
+}
