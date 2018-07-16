e74c5d242cfb5177f7b3508889af1ba81b74a8ba
zhayu <zhayu.ll@taobao.com>
2018-07-16 15:23:03 +0800

    升级到2.3.3.rc70

--- atlas-gradle-plugin/atlas-plugin/src/main/java/com/taobao/android/builder/AtlasPlugin.java
+++ atlas-gradle-plugin/atlas-plugin/src/main/java/com/taobao/android/builder/AtlasPlugin.java
@@ -249,25 +249,7 @@ public class AtlasPlugin extends AtlasBasePlugin {
         {
             @Override
             public void execute(Project project) {
-                
-                if (PluginTypeUtils.isAppProject(project) && atlasExtension.isAtlasEnabled()) {
 
-                    Map<String, String> multiDex = new HashMap<>();
-                    multiDex.put("group", "com.android.support");
-                    multiDex.put("module", "multidex");
-                    project.getConfigurations().all(new Action<Configuration>() {
-                        @Override
-                        public void execute(Configuration configuration) {
-                            configuration.exclude(multiDex);
-                        }
-                    });
-
-                    if (AtlasBuildContext.sBuilderAdapter.addAtlasDependency){
-                        project.getDependencies().add("compile", "com.taobao.android:atlasupdate:1.1.4.5");
-                        project.getDependencies().add("compile", "com.taobao.android:atlas_core:5.0.6-rc21@aar");
-                    }
-
-                }
 
                 //3. update extension
                 atlasConfigurationHelper.updateExtensionAfterEvaluate();
