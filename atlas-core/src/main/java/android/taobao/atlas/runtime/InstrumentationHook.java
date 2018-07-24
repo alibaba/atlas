16794c1989ee241afc922c04c2e59053ab0bd09c
chenzhong.cz <chenzhong.cz@alibaba-inc.com>
2018-07-24 14:36:25 +0800

    [atlas-core] 有错误就要报错

--- atlas-core/src/main/java/android/taobao/atlas/runtime/InstrumentationHook.java
+++ atlas-core/src/main/java/android/taobao/atlas/runtime/InstrumentationHook.java
@@ -571,7 +571,8 @@ public class InstrumentationHook extends Instrumentation {
 	        	intent.putExtra("android.taobao.atlas.mainAct.wait", false);
 	        }
         }catch (Exception e){
-        }
+			e.printStackTrace();
+		}
 
         try {
 			// BundleListing.BundleInfo info = AtlasBundleInfoManager.instance().getBundleInfo(AtlasBundleInfoManager
