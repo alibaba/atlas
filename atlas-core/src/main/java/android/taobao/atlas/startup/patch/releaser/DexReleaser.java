bd4e8f490b0e9cc3f8e41c27c76e86c20382f813
查郁 <zhayu.ll@taobao.com>
2019-01-02 14:19:01 +0800

    atlas_core 5.1.0.8-RC3 atlasupdate 1.1.4.23

--- atlas-core/src/main/java/android/taobao/atlas/startup/patch/releaser/DexReleaser.java
+++ atlas-core/src/main/java/android/taobao/atlas/startup/patch/releaser/DexReleaser.java
@@ -259,7 +259,7 @@ public class DexReleaser {
             }
 
 
-            if (RuntimeVariables.patchVersion == 2 ||!isArt()) {
+            if (!isArt()) {
                 Enumeration entryEnumeration = zipFile.entries();
                 while (entryEnumeration.hasMoreElements()) {
                     ZipEntry zipEntry = (ZipEntry) entryEnumeration.nextElement();
@@ -270,28 +270,25 @@ public class DexReleaser {
                         fileOutputStream.close();
                     }
                 }
-            }else
-
-
-//                if (RuntimeVariables.patchVersion == 1) {
-//                File sourceFile;
-//                if (dexPatch) {
-//                    //如果kernalBundle==null，说明主dex没有被升级过，那么就和base.apk做merge
-//                    sourceFile = null == KernalBundle.kernalBundle
-//                            ? new File(KernalConstants.APK_PATH)
-//                            : KernalBundle.kernalBundle.getRevisionZip();
-//                } else {
-//                    sourceFile = new File(KernalConstants.APK_PATH);
-//                }
-//                File targetFile = File.createTempFile(sourceFile.getName(), ".tmp", bundleFile.getParentFile());
-//                CombineDexMerger combineDexMerger = new CombineDexMerger(new CombineDexVerifier());
-//                boolean result = combineDexMerger.merge(sourceFile, bundleFile, targetFile);
-//                if (result && bundleFile.delete() && targetFile.renameTo(bundleFile)) {
-//                    return true;
-//                } else {
-//                    return false;
-//                }
-//            }
+            }else {
+                File sourceFile;
+                if (dexPatch) {
+                    //如果kernalBundle==null，说明主dex没有被升级过，那么就和base.apk做merge
+                    sourceFile = null == KernalBundle.kernalBundle
+                            ? new File(KernalConstants.APK_PATH)
+                            : KernalBundle.kernalBundle.getRevisionZip();
+                } else {
+                    sourceFile = new File(KernalConstants.APK_PATH);
+                }
+                File targetFile = File.createTempFile(sourceFile.getName(), ".tmp", bundleFile.getParentFile());
+                CombineDexMerger combineDexMerger = new CombineDexMerger(new CombineDexVerifier());
+                boolean result = combineDexMerger.merge(sourceFile, bundleFile, targetFile);
+                if (result && bundleFile.delete() && targetFile.renameTo(bundleFile)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
 
                 return true;
 //            } else {
