9df971fa6fa03653cc6f885aef80b65b1c904439
查郁 <zhayu.ll@taobao.com>
2019-04-25 14:05:02 +0800

    提交

--- atlas-gradle-plugin/atlas-plugin/src/main/java/com/taobao/android/builder/hook/dex/DexByteCodeConverterHook.java
+++ atlas-gradle-plugin/atlas-plugin/src/main/java/com/taobao/android/builder/hook/dex/DexByteCodeConverterHook.java
@@ -238,7 +238,11 @@ public class DexByteCodeConverterHook extends DexByteCodeConverter {
                 FileCache.Inputs inputsKey = globalCacheBuilder.putString("md5", MD5Util.getFileMd5(inputFile)).build();
 
                 try {
-                    fileCache.createFile(outDexFolder, inputsKey, () -> DexByteCodeConverterHook.super.convertByteCode(inputFile, outDexFolder, multidex, mainDexList, dexOptions, processOutputHandler, minSdkVersion));
+                    fileCache.createFile(outDexFolder, inputsKey, () -> {
+                        outDexFolder.mkdirs();
+                        DexByteCodeConverterHook.super.convertByteCode(inputFile, outDexFolder, multidex, mainDexList, dexOptions, processOutputHandler, minSdkVersion);
+                    });
+
                 } catch (ExecutionException e) {
                     e.printStackTrace();
                     failures.add(e);
